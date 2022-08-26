a8ab03be536a0044c8b359bb2e494d1adf984eac
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-26 09:53:32 -0600

    Add files via upload

--- Not person project (not human, mark zuckerburg)/goverment.java
+++ Not person project (not human, mark zuckerburg)/goverment.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Math;
 public class goverment {
     /*
      * TYPES: 
@@ -6,15 +10,60 @@ public class goverment {
      * 2 = Democracy
      * 3 = Dictators
      */
-    int type,wealth;
-    human peoples[];
+    int type,wealth,health;
+    goverment enemy;
+    List<human> peoples =  new ArrayList<human>();
     human leader;
+    String name;
     String types[] = {"Anarchy","Monarchy","Democracy","Dictatorship"};
-    goverment(int type, int wealth, human leader){
+    public void printf(Object e){
+        System.out.println(e);
+    }
+    goverment(int type, int wealth, human leader, String name){
         this.type = type;
         this.wealth = wealth;
         this.leader = leader;
+        this.name = name;
+        health = 100;
         System.out.println("A new goverment has been formed with type: "+types[type]+", and with leader: "+leader.name);
     }
-    
+    public void update(){
+        System.out.println(name+ ", is a goverment with type: "+types[type]+", and with leader: "+leader.name+" has peoples: ");
+        int count = 0;
+        for(int i = 0; i < peoples.size();i++){
+            if(peoples.get(i).dead == false){
+                System.out.println(count+". "+peoples.get(i).name);
+                count++;
+            }
+        }
+        System.out.println("---------------------------------------------");
+    }
+    public void addPerson(human person){
+        peoples.add(person);
+        System.out.println(name + ", has a new member, "+person.name);
+        this.update();
+    }
+    public void toWar(goverment enemy){
+        this.enemy = enemy;
+        System.out.println(name + " has declared war on: "+enemy.name);
+        if(enemy.enemy != this){
+            enemy.toWar(this);
+        }
+    }
+    public void attack(){
+        if(health > 0){
+            int number = (int)(Math.random()*100+1);
+            number -= enemy.wealth;
+            printf(name + " has attacked " + enemy.name + " with a "+number+"% chance of hitting");
+            if(number < 50){
+                printf("FAILED!");
+            }else{
+                printf("HIT! XD LAMO!");
+                enemy.health -= number;
+                printf(enemy.name + " now has "+enemy.health+" health left.");
+            }
+        }else{
+            
+        }
+    }
 }
--- Not person project (not human, mark zuckerburg)/main.java
+++ Not person project (not human, mark zuckerburg)/main.java
@@ -4,7 +4,37 @@ public class main{
         testhuman.say("Hi");
         human trevor = new human("Trevor",36,125,false,false,true);
         trevor.say("Imma start a dictatorship lamo");
-        goverment trevorland = new goverment(3,0,trevor);
+        // Trevorland
+        goverment trevorland = new goverment(3,0,trevor,"Trevorland");
+        testhuman.say("I should join trevorland?");
+        trevorland.addPerson(testhuman);
+        testhuman.say("*insert bad joke here*");
+        testhuman.die();
+        
+        //new human
+        human logan = new human("Logan",414,1433,false,false,true);
+        trevorland.addPerson(logan);
+        
+        //new enemy goverment
+        human chinmay = new human("Chinmay Tiwari",245,142,false,false,true);
+        goverment creator = new goverment(3,0,chinmay,"creator");
+        
+        //trevor goes to war
+        trevor.say("I will rage war against the creator!");
+        trevorland.toWar(creator);
+        logan.say("Attacking on frontlines");
+        for(int i = 0; i < 25; i++){
+            trevorland.attack();
+            creator.attack();
+            if(trevorland.health < 0){
+                trevorland.printf("Trevorland has ended!");
+                break;
+            }
+            if(creator.health < 0){
+                trevorland.printf("Creator has ended!");
+                break;
+            }
+        }
     }
     
 }
