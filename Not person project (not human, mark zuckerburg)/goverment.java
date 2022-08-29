280ef638313f8594ac284dae5e51cecadf38e99e
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-29 12:33:35 -0600

    Update goverment.java

--- Not person project (not human, mark zuckerburg)/goverment.java
+++ Not person project (not human, mark zuckerburg)/goverment.java
@@ -31,7 +31,7 @@ public class goverment {
         System.out.println("A new goverment has been formed with type: "+types[type]+", and with leader: "+leader.name);
     }
     public void attack(){
-        if(health > 0){
+        if(health > 0 && atwar == true){
             int number = (int)(Math.random()*100+1);
             number -= enemy.wealth;
             if(enemy.health > 0){
@@ -39,18 +39,23 @@ public class goverment {
                 if(number < 50){
                     printf("Attacked failed!");
                 }else{
-                    printf("Attack successe!");
+                    printf("Attack success!");
                     enemy.health -= number;
                     printf(enemy.name + " now has "+enemy.health+" health left.");
+                  if(enemy.health < 0){
+                    printf(enemy.name + " has fallen!");
+                  }
                 }
             }else{
                 printf(enemy.name + " has fallen, yet "+name+" still tried to attack!!");
             }
         }else{
             printf(name+" has fallen!");
+            atwar = false;
         }
     }
     public void update(List<goverment> goverments){
+        if(health > 0){
         System.out.println(name+ ", is a goverment with type: "+types[type]+", and with leader: "+leader.name+" has peoples: ");
         int count = 0;
         for(int i = 0; i < peoples.size();i++){
@@ -62,39 +67,51 @@ public class goverment {
         Random rand = new Random();
         int spy = rand.nextInt((peoples.size() - 0) + 1) + 0;
         try{
-        printf("'Bird' has spyed and got : "+spyone.spy(peoples.get(spy)));
+        printf("'Bird' has spied and got : "+spyone.spy(peoples.get(spy)));
         }catch(Exception e){
             printf("Failed in spy mission. Maybe next time?");
         }
         diplomacy(goverments);
         if(atwar){
-            attack(this.enemy);
+            if(enemy.health > 0 && health > 0){
+              attack();
+            }
         }
         System.out.println("---------------------------------------------");
+      }
     }
     public void addPerson(human person){
         peoples.add(person);
         System.out.println(name + ", has a new member, "+person.name);
     }
     public void toWar(goverment enemy){
+      if(enemy.enemy != this){
+            if(enemy.health > 0){
         this.enemy = enemy;
         System.out.println(name + " has declared war on: "+enemy.name);
         atwar = true;
-        if(enemy.enemy != this){
+        
             enemy.toWar(this);
+            
         }
+      }
     }
     
     public void diplomacy(List<goverment> goverments){
         for (int i = 0; i < goverments.size(); i++){
-            talk(goverments.get(i));
+            if(goverments.get(i).name != name){
+              talk(goverments.get(i));
+            }
         }
     }
     public void talk(goverment other){
         Random rand = new Random();
         int agre = rand.nextInt((9*4 - 0) + 1) + 0;
-        if(agre > other.agression){
+        if(agre >= other.agression){
             toWar(other);
+        }else{
+          
+          printf("Diplomacy went well.");
         }
     }
 }
